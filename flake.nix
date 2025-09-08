9979e1e4b84a26d843cca7d76383c0fb4a0f8539
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-08 19:24:08 -0400

    style: treefmt

--- flake.nix
+++ flake.nix
@@ -3,10 +3,18 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
@@ -30,5 +38,6 @@
             ];
           };
         };
-      });
+      }
+    );
 }
