38eedd7c061d885651c8964dd3e2f41ae4d656d8
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-09 03:40:00 -0400

    feat(backend): add production database connection

--- backend/src/orbtronics_l1_software_engineer_backend/helpers/database.py
+++ backend/src/orbtronics_l1_software_engineer_backend/helpers/database.py
@@ -2,3 +2,5 @@ import pymongo
 from orbtronics_l1_software_engineer_backend.helpers import environment
 
 client = pymongo.MongoClient(environment.MONGO_CONNECTION_URI)
+
+production_database = client[str(environment.MONGO_DATABASE_NAME)]
