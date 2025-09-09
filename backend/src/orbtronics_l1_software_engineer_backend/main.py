091f1f005db814fe7812d01957d42ae6d0cd4994
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-09 03:43:36 -0400

    feat(backend): add application entrypoint
    - setup based on openapi specification

---
+++ backend/src/orbtronics_l1_software_engineer_backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from orbtronics_l1_software_engineer_backend.routes import (auth, health,
+                                                            tasks, users)
+
+app = FastAPI(
+    title="Orbtronics L1 Software Engineer Backend",
+    description="Technical Design Round Backend",
+    version="0.1.0",
+    openapi_tags=[
+        {"name": "users", "description": "user accounts"},
+        {"name": "tasks", "description": "user tasks"},
+    ],
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+)
