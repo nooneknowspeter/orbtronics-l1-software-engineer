0eee4c05beb41b38494e0437fe20f3956b8aa470
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-10 06:12:12 -0400

    refactor(backend): update environment variables in database helper

--- backend/src/orbtronics_l1_software_engineer_backend/helpers/database.py
+++ backend/src/orbtronics_l1_software_engineer_backend/helpers/database.py
@@ -1,7 +1,7 @@
 import pymongo
 from orbtronics_l1_software_engineer_backend.helpers import environment
 
-client = pymongo.MongoClient(environment.MONGO_CONNECTION_URI)
+client = pymongo.MongoClient(environment.variables.get("MONGO_CONNECTION_URI"))
 
-production_database = client[str(environment.MONGO_DATABASE_NAME)]
+production_database = client[str(environment.variables.get("MONGO_DATABASE_NAME"))]
 test_database = client["test"]
