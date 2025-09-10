def286719dc9e384b7af1a82508595425518c4bf
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-10 18:39:08 -0400

    refactor(backend): update tasks schemas

--- backend/src/orbtronics_l1_software_engineer_backend/schemas/tasks.py
+++ backend/src/orbtronics_l1_software_engineer_backend/schemas/tasks.py
@@ -1,29 +1,52 @@
+from datetime import date, datetime
+from enum import Enum
+
 from pydantic import BaseModel
 
 
+class TaskPriorityEnum(str, Enum):
+    none = "none"
+    low = "low"
+    medium = "medium"
+    high = "high"
+
+
+class TaskStatusEnum(str, Enum):
+    todo = "todo"
+    in_progress = "in_progress"
+    done = "done"
+
+
+class TaskDueEnum(Enum):
+    none = "none"
+    today = "today"
+    week = "week"
+    overdue = "overdue"
+
+
 class TaskData(BaseModel):
-    taskId: str
-    userId: str
+    task_id: str
+    user_id: str
     title: str
     description: str | None = None
-    priority: str | None = None
     status: str
-    dueDate: str | None = None
-    createdAt: str | None = None
-    updatedAt: str | None = None
+    priority: str | None = None
+    due_date: str | None = None
+    created_at: str
+    updated_at: str
 
 
 class TaskDataInput(BaseModel):
     title: str
-    status: str
     description: str | None = None
+    status: str = TaskStatusEnum.todo
     priority: str | None = None
-    dueDate: str | None = None
+    due_date: date | str | None = None
 
 
 class TaskDataPartial(BaseModel):
     title: str | None
     description: str | None
-    priority: str | None
     status: str | None
-    dueDate: str | None
+    priority: str | None
+    due_date: str | None
