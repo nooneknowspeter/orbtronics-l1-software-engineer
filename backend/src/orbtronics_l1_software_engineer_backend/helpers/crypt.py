ad82146fd1fc0afad93fa5b98a6824fd9e9900b6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-10 06:39:20 -0400

    feat(backend): add jwt token cryptography helpers

--- backend/src/orbtronics_l1_software_engineer_backend/helpers/crypt.py
+++ backend/src/orbtronics_l1_software_engineer_backend/helpers/crypt.py
@@ -1,3 +1,10 @@
+from datetime import datetime, timedelta, timezone
+from typing import Any
+
+import jwt
+from fastapi import HTTPException, status
+from jwt import PyJWTError
+from orbtronics_l1_software_engineer_backend.helpers import environment
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -9,3 +16,38 @@ def hashPassword(password: str) -> str:
 
 def verifyPassword(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
+
+
+def createAccessToken(subject: str, expires_delta: timedelta | None = None) -> str:
+    creation_time = datetime.now(timezone.utc)
+
+    if expires_delta is None:
+        expires_delta = timedelta(
+            minutes=float(
+                str(environment.variables.get("ACCESS_TOKEN_EXPIRES_MINUTES"))
+            )
+        )
+    to_encode = {
+        "sub": subject,
+        "iat": int(creation_time.timestamp()),
+        "exp": int((creation_time + expires_delta).timestamp()),
+    }
+
+    return jwt.encode(
+        to_encode,
+        environment.variables.get("JWT_SECRET"),
+        algorithm=str(environment.variables.get("JWT_ALGORITHM")),
+    )
+
+
+def decodeAccessToken(token: str) -> dict[str, str]:
+    try:
+        return jwt.decode(
+            token,
+            environment.variables.get("JWT_SECRET"),
+            algorithms=[str(environment.variables.get("JWT_ALGORITHM"))],
+        )
+    except PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid token"
+        )
