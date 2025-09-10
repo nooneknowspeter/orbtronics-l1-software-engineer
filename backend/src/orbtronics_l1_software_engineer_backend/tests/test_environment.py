90b8cc82b05d0247f4ac47c7d626a3417dc407b6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-10 06:10:46 -0400

    test(backend): update environment variable tests

--- backend/src/orbtronics_l1_software_engineer_backend/tests/test_environment.py
+++ backend/src/orbtronics_l1_software_engineer_backend/tests/test_environment.py
@@ -1,15 +1,9 @@
-import pytest
 from orbtronics_l1_software_engineer_backend.helpers import environment
 
 
 def test_environmentVariables():
-    assert environment.FRONTEND_URL != ""
-    assert environment.SERVER_PORT != ""
-    assert environment.JWT_SECRET != ""
-    assert environment.JWT_ALGORITHM != ""
-    assert environment.ACCESS_TOKEN_EXPIRES_MINUTES != ""
-    assert environment.COOKIE_NAME != ""
-    assert environment.MONGO_INITDB_ROOT_USERNAME != ""
-    assert environment.MONGO_INITDB_ROOT_PASSWORD != ""
-    assert environment.MONGO_DATABASE_NAME != ""
-    assert environment.MONGO_CONNECTION_URI != ""
+    print()
+    for key in environment.variables:
+        assert environment.variables.get(key) != ""
+
+        print(f"{key}: {environment.variables[key]}")
