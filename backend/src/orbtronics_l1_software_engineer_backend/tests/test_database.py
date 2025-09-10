76a50140c91a4d3403249d8a1fb8b63519b1d130
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-10 06:12:34 -0400

    test(backend): update database tests

--- backend/src/orbtronics_l1_software_engineer_backend/tests/test_database.py
+++ backend/src/orbtronics_l1_software_engineer_backend/tests/test_database.py
@@ -1,6 +1,3 @@
-import os
-
-import pytest
 from orbtronics_l1_software_engineer_backend.helpers.database import \
     test_database
 
@@ -10,11 +7,19 @@ collection = test_database["test_collection"]
 def test_databaseConnection():
     assert test_database.name == "test"
 
+    print()
+    print(f"database_name: {test_database.name}")
+
 
 def test_createDatabaseDocument():
-    test_document = {"key": "test"}
-    test_insert = collection.insert_one(test_document)
-    assert (
-        collection.find_one({"_id": test_insert.inserted_id})["key"]
-        == test_document["key"]
+    expected_document = {"key": "test"}
+    test_database_entry = collection.insert_one(expected_document)
+    test_database_document = collection.find_one(
+        {"_id": test_database_entry.inserted_id}
     )
+
+    assert test_database_document["key"] == expected_document["key"]
+
+    print()
+    for key in test_database_document:
+        print(f"{key}: {test_database_document[key]}")
