707c342101c8886d84bf729d56fcbc5e5e8d146f
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-11 23:50:49 -0400

    chore(infra): add ingress rule to firewall
    - ingress rule for database admin access

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "this" {
   }
 
   ingress {
+    description = "database admin"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "ssh"
     from_port   = 22
     to_port     = 22
