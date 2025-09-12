23179afab2871e0e91bc40505f0f2c01de42522c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-11 22:09:57 -0400

    chore(infra): add networking infrastructure as code
    - vpc
    - gateway
    - routing
    - subnet
    - firewall

---
+++ infrastructure/networking.tf
@@ -0,0 +1,56 @@
+resource "aws_vpc" "this" {
+  cidr_block                       = "192.168.0.0/24"
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "this" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 1, 1)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.this.id
+}
+
+resource "aws_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    description = "backend"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
