81948a9e3b93b3583abb905a793580051bcfc0d6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-12 05:20:50 -0400

    style: treefmt

--- infrastructure/scripts/extract-pem-key.sh
+++ infrastructure/scripts/extract-pem-key.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 if command -v terraform; then
-	echo "command terraform not found"
+  echo "command terraform not found"
 fi
 
 echo "extracting private key"
 
 if command -v cat private-key.pem; then
-	chmod 600 private-key.pem 
-	touch private-key.pem
+  chmod 600 private-key.pem
+  touch private-key.pem
 fi
 
 terraform output -raw aws_key_pair_private_key > private-key.pem
