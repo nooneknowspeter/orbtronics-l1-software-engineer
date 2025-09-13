ad4ca3c503c23f0a16ecdb40a910eb27a4b19948
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-13 10:20:21 -0400

    chore(infra): add ingress firewall rule
    - port forward frontend application

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
 
   ingress {
+    description = "frontend"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "backend"
     from_port   = 8000
     to_port     = 8000
