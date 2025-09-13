a653ff5f40759fc585e9fb7c8697f9676bd98cea
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-13 08:39:49 -0400

    feat(frontend/components): add components barrel file

---
+++ frontend/src/components/components.tsx
@@ -0,0 +1,6 @@
+import Navbar from "./navbar/navbar";
+import AuthModal from "./modal/auth";
+import ThemeController from "./swap/theme-controller";
+import App from "./viewport/app";
+
+export { Navbar, AuthModal, ThemeController, App };
