f1921dea11c9f17a9810f873a1d971c84284baa9
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-14 07:47:36 -0400

    feat(frontend): add context barrel file

---
+++ frontend/src/context/context.tsx
@@ -0,0 +1,5 @@
+import { AuthProvider } from "./auth-context";
+import { TaskProvider } from "./task-context";
+import { UserProvider } from "./user-context";
+
+export { AuthProvider, TaskProvider, UserProvider };
